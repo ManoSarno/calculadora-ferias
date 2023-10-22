b4955343029ab10bd76460aa1cd6427989a27191
ManoSarno <manusarno97@gmail.com>
2023-10-22 14:36:25 -0300

    fixing border break on result table

--- components/TableRow.tsx
+++ components/TableRow.tsx
@@ -10,16 +10,16 @@ export default function TableRow({column1, column2, column3, column4, lastRow}: 
   return (
     <>
       {lastRow ? (
-        <div className="grid grid-cols-2">
-          <p className="text-left pt-4 pb-5 px-5 my-auto">{column1}</p>
-          <p className="bg-light-yellow text-center pt-4 pb-5 px-5 my-auto">{column2}</p>
+        <div className="grid grid-cols-2 h-12">
+          <p className="text-left pb-1 px-5 my-auto">{column1}</p>
+          <p className="flex bg-light-yellow text-center px-5 h-full"><span className="m-auto">{column2}</span></p>
         </div>
       ) : (
-        <div className="grid grid-cols-[3fr_1fr_2fr_2fr]">
-          <p className="text-left py-4 pl-5 pr-2 border-b border-light-blue my-auto">{column1}</p>
-          <p className="text-center py-4 px-2 border-b border-light-blue my-auto">{column2}</p>
-          <p className="text-center py-4 px-2 border-b border-light-blue my-auto">{column3}</p>
-          <p className="text-center py-4 pr-5 pl-2 border-b border-light-blue my-auto">{column4}</p>
+        <div className="grid grid-cols-[3fr_1fr_2fr_2fr] h-12 border-b border-light-blue">
+          <p className="text-left pl-5 pr-2 my-auto">{column1}</p>
+          <p className="text-center px-2 my-auto">{column2}</p>
+          <p className="text-center px-2 my-auto">{column3}</p>
+          <p className="text-center pr-5 pl-2 my-auto">{column4}</p>
         </div>
       )}
     </>
